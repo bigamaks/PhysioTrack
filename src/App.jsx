c4fd5a4903c93c1648a7aaf20c6078b71cd5a952
Chiamaka Aniago <chiamakaaniago@gmail.com>
2026-07-29 20:50:31 +0100

    Initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './index.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold p-8 bg-red-300">
+      Welcome to TheraTrack 👋
+    </h1>
+    </>
+  )
+}
+
+export default App
